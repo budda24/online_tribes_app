3f0fa9ebeaa9376451d0742fbd3fc902daf2d647
Emil <ezmil34@gmail.com>
2022-07-13 17:16:59 +0200

    refactor registartion controller

--- lib/app/modules/registration/controllers/registration_controller.dart
+++ lib/app/modules/registration/controllers/registration_controller.dart
@@ -1,7 +1,5 @@
 import 'dart:io' as io;
 
-import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/controllers/global_controler.dart';
 import 'package:flutter_application_1/infrastructure/fb_services/auth/auth_services.dart';
@@ -19,20 +17,19 @@ class RegistrationController extends GetxController {
   final globalController = Get.find<GlobalController>();
   final cameraController = Get.put(CameraController());
 
-  /* GlobalKey<FormState> formKey = GlobalKey(); */
-
-  final signUpTribalNameController = TextEditingController();
-  final signUpPhoneController = TextEditingController();
-  final smsCodeController = TextEditingController();
-  final signUpPasswordConfirmController = TextEditingController();
-
-  RxDouble sliderValue = 1.0.obs;
-  final auth = FirebaseAuth.instance;
-  String verificationID = '';
-  bool isSMSCodeHere = false;
-  bool isLoadingVisible = false;
+  final TextEditingController describeYourselfController =
+      TextEditingController();
+  final TextEditingController lifeMottoController = TextEditingController();
+  final TextEditingController hobbyController = TextEditingController();
+  final TextEditingController timeToInvestController = TextEditingController();
 
   io.File? profilePicture;
+
+  RxDouble sliderValue = 1.0.obs;
+
+  UserDB userDB = UserDB(
+    userId: currentUser.uid,
+  );
 
   uploadFile({required String fileName, required String directory}) {
     //TODO cloud function to resize photo to secure the end point
@@ -46,27 +43,15 @@ class RegistrationController extends GetxController {
         fileName: '$fileName${extension(profilePhoto.path)}');
   }
 
-  /* uploadProfileVideo() {
-    //TODO cloud function to resize photo to secure the end point
-    const storage = UserCloudStorageServices();
-    String userId = auth.currentUser!.uid;
-    var profilePhoto = cameraController.pickedFile!;
-    storage.uploadFile(
-        path: 'profile',
-        userId: userId,
-        imageToUpload: profilePhoto,
-        fileName: 'profile_video${extension(profilePhoto.path)}');
-  } */
-
-  final TextEditingController describeYourselfController =
-      TextEditingController();
-  final TextEditingController lifeMottoController = TextEditingController();
-  final TextEditingController hobbyController = TextEditingController();
-  final TextEditingController timeToInvestController = TextEditingController();
-
-  UserDB userDB = UserDB(
-    userId: currentUser.uid,
-  );
+  String? userDBValidator({required String value, required int lenght}) {
+    if (value.isEmpty) {
+      return 'write some text';
+    }
+    if (value.length > lenght) {
+      return 'Max message lenght = ${lenght.toString()} char';
+    }
+    return null;
+  }
 
   Future<void> saveNewUser() async {
     userDB.description = describeYourselfController.text;
@@ -80,20 +65,8 @@ class RegistrationController extends GetxController {
     await UserDBServices().createUser(userDB);
   }
 
-  String? userDBValidator({required String value, required int lenght}) {
-    if (value.isEmpty) {
-      return 'write some text';
-    }
-    if (value.length > lenght) {
-      return 'Max message lenght = ${lenght.toString()} char';
-    }
-    return null;
-  }
 
   closeKeyboard() {
     globalController.unFocuseNode();
   }
-
-  @override
-  void onClose() {}
 }
